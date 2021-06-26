bb5db95adc1ff303367a7dd7fcaa5175b4eaa4a1
sidinsomniac <chatterjeesid1993@gmail.com>
2021-06-26 18:22:08 +0530

    Create login controller

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,28 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const logger = require("../utils/logger");
+
+const { register, login } = require("../controllers/user");
+
 const catchAsyncError = require("../utils/catchAsyncError");
 
-const User = require("../models/user");
+router.post("/login", catchAsyncError(login));
 
-router.post("/login", (req, res) => {
-    res.send("login");
-});
-
-router.post("/register", catchAsyncError(
-    async (req, res, next) => {
-        const { body: { username, email, password } } = req;
-        const newUser = new User({
-            username,
-            email,
-            password
-        });
-        res.json(newUser);
-        // await newUser.save();
-        // logger.info("New user created", newUser);
-        // res.status(201).json(newUser);
-    }
-));
-
+router.post("/register", catchAsyncError(register));
 
 module.exports = router;
